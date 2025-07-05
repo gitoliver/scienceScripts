e09fc0a3af2e090709c4598729c4b4701bce3cdd
gitoliver <olgrant@uga.edu>
2025-07-05 19:44:50 +0200

    Writes how it was called. Writes more info. Does not write a PDB.

--- ERManISuperimpositionWithBFMPFiltering/a.aligner_WithLimit_AndFilter.sh
+++ ERManISuperimpositionWithBFMPFiltering/a.aligner_WithLimit_AndFilter.sh
@@ -2,6 +2,7 @@ if [ $# != 6 ]; then
     echo "Usage: $0 simReplicates.txt inputFolder outputFolder alignerInputs.txt filterFile.txt alignmentResidue"
     exit
 fi
+echo "$0 called with simReplicates.txt:$1 ,inputFolder:$2 ,outputFolder:$3 ,alignerInputs.txt:$4 ,filterFile.txt:$5 ,alignmentResidue:$6"
 simReplicates=$1
 inputFolder=$2
 outputFolder=$3
@@ -24,6 +25,7 @@ do
     echo "open $REFERENCE_3D_STRUCTURE" > $simReplicate-input.cmd
     fileCount=1
     RELATIVE_PATH_TO_SNAPSHOTS=`echo "$TEMPLATE_PATH_TO_SNAPSHOTS" | sed "s,SYSTEM,$inputFolder/$simReplicate,g"`
+    echo "RELATIVE_PATH_TO_SNAPSHOTS:${RELATIVE_PATH_TO_SNAPSHOTS}"
     snapshotNumber=0
     for filePath in `ls $RELATIVE_PATH_TO_SNAPSHOTS`
     do
@@ -37,7 +39,7 @@ do
             echo "findclash $REFERENCE_OVERLAP_SELECTION test #$fileCount$SYSTEM_PROTEIN_OVERLAP_RESIDUES saveFile $simReplicate-ss$snapshotNumber-proteinOverlaps.txt" >> $simReplicate-input.cmd
             echo "findclash $REFERENCE_OVERLAP_SELECTION test \"#$fileCount$SYSTEM_PROTEIN_OVERLAP_RESIDUES | #$fileCount:$FIRST_GLYCAN_RESIDUE-$LAST_GLYCAN_RESIDUE & ~#$fileCount:$nextNagResidue-$thisGlycanLastResidueNumber\" saveFile $simReplicate-ss$snapshotNumber-glycanOverlaps.txt" >> $simReplicate-input.cmd
             echo "findclash $REFERENCE_OVERLAP_SELECTION test \"#$fileCount$SYSTEM_PROTEIN_OVERLAP_RESIDUES | #$fileCount:4YB,VMB & ~#$fileCount:$nextNagResidue-$thisGlycanManBResidueNumber\" saveFile $simReplicate-ss$snapshotNumber-glycanCoreOverlaps.txt" >> $simReplicate-input.cmd
-            echo "write #$fileCount $simReplicate-ss$snapshotNumber-aligned.pdb" >> $simReplicate-input.cmd
+            #echo "write #$fileCount $simReplicate-ss$snapshotNumber-aligned.pdb" >> $simReplicate-input.cmd
             fileCount=$(($fileCount + 1))
             if [ $fileCount -gt $STRUCTURE_LIMIT_CHIMERA ]; then
                 echo "Running chimera for $simReplicate"
